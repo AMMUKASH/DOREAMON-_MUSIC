2dc37371fbcc6249958eefdf5fe708e7cf484a8e
STARK <misssqn@gmail.com>
2026-02-28 11:43:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,31 +1,34 @@
 import re
 from os import getenv
-
 from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+# Aapki Details Yahan Add Kar Di Hain
+API_ID = int(getenv("API_ID", "34135757"))
+API_HASH = getenv("API_HASH", "d3d5548fe0d98eb1fb793c2c37c9e5c8")
+BOT_TOKEN = getenv("BOT_TOKEN", "8541688649:AAHLhJLdVk9lT6uOsD-u4qfCY0wmM2RUiaY")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://misssqn:VICTOR01@cluster0.3otqmso.mongodb.net/?appName=Cluster0")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 10000))
 
-LOGGER_ID = int(getenv("LOGGER_ID", None))
-OWNER_ID = int(getenv("OWNER_ID", 7403621976))
+# Logger aur Owner ID
+LOGGER_ID = int(getenv("LOGGER_ID", "-1008581811595"))
+OWNER_ID = int(getenv("OWNER_ID", "8581811595"))
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TEAMPURVI/ALPHA_MUSIC")
+# Upstream Repo (Aapki apni repo ka link bhi daal sakte hain yahan)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AMMUKASH/DOREAMON-_MUSIC")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_BOTS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
+# Support Links
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/radhesupport")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+PKYLDIEYiTljMzMx")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
@@ -37,11 +40,8 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
+# String Session (Ise Render ke Env Var mein daalna mat bhulna)
 STRING1 = getenv("STRING_SESSION", None)
-STRING2 = getenv("STRING_SESSION2", None)
-STRING3 = getenv("STRING_SESSION3", None)
-STRING4 = getenv("STRING_SESSION4", None)
-STRING5 = getenv("STRING_SESSION5", None)
 
 BANNED_USERS = filters.user()
 adminlist = {}
@@ -50,8 +50,9 @@ votemode = {}
 autoclean = []
 confirmer = {}
 
-START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/fu6jk3.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/26nzoq.jpg")
+# Images (Aapki Image Update Kar Di Hai)
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/fc78336c4402d1f53d696-95139de88bc561d55a.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org/file/fc78336c4402d1f53d696-95139de88bc561d55a.jpg")
 PLAYLIST_IMG_URL = "https://files.catbox.moe/f2s4ws.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/z0gh23.jpg"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/2y5o3g.jpg"
@@ -72,8 +73,8 @@ DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 if SUPPORT_CHANNEL:
     if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
-        raise SystemExit("[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://")
+        raise SystemExit("[ERROR] - Your SUPPORT_CHANNEL url is wrong.")
 
 if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
-        raise SystemExit("[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://")
+        raise SystemExit("[ERROR] - Your SUPPORT_CHAT url is wrong.")
